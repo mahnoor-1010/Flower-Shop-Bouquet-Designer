3af971fc748c26308e8cf6f2952f8cc2438b3da0
Mahnoor Faisal <u2025367@giki.edu.pk>
2025-11-29 16:49:41 +0300

    added functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,62 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+int optionMain;
 void mainMenu(){
-  cout << "hello" << endl;
+  cout << endl;   
+  cout << "1. Customer Login" << endl;
+  cout << "2. Florist Login" << endl;
+  cout << "3. Admin Login" << endl;
+  cout << "4. Exit" << endl;
+  cout << "Select an option: ";
+  cin >> optionMain;
+}
+
+int optionCustomer;
+void customerMenu(){
+  cout << endl;
+  cout << "1. Create a Bouquet" << endl;
+  cout << "2. View Cart" << endl;
+  cout << "3. Place Order" << endl;
+  cout << "4. View Order History" << endl;
+  cout << "5. Update Profile" << endl;
+  cout << "6. Logout" << endl;
+  cout << "Select an option: ";
+  cin >> optionCustomer;
+}
+int optioncreateabouquet;
+void optioncreateabouquet(){
+  cout << "1. Add Flower" << endl;
+  cout << "2. View Bouquet" << endl;
+  cout << "3. Checkout" << endl;
+  cout << "4. Cancel" << endl;
+  cin >> optioncreateabouquet;
+  
+}
+
+void optionviewbouquet(){
+  cout << "1. Remove a flower" << endl;
+  cout << "2. Add a flower" << endl;
+  cout << "3. Checkout" << endl;
+  cout << "4. Cancel" << endl;
+  cin >> optionviewbouquet;
 }
 int main(){
+string bouquetName;
 
-  cout << "Change by Mahnoor";
-
-
+ cout << endl;
+    cout << "=== Welcome to Flower Shop ===" << endl;
+    mainMenu();
+    if(optionMain == 1){
+        
+        if(optionCustomer == 1){
+          cout << "Name your bouquet: ";
+          cin >> bouquetName;
+          cout << "Available Flowers: ";
+            
+        }
+    }
 
   return 0;
 }
