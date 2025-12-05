65254a0aa53714bc11b0f3f954561cbe09458766
Mahnoor Faisal <u2025367@giki.edu.pk>
2025-12-05 10:00:01 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -26,37 +26,79 @@ void customerMenu(){
   cin >> optionCustomer;
 }
 int optioncreateabouquet;
-void optioncreateabouquet(){
+void createabouquet(){
   cout << "1. Add Flower" << endl;
   cout << "2. View Bouquet" << endl;
   cout << "3. Checkout" << endl;
   cout << "4. Cancel" << endl;
+  cout << "Select an option: ";
   cin >> optioncreateabouquet;
   
 }
-
-void optionviewbouquet(){
+int optionviewbouquet;
+void viewbouquet(){
   cout << "1. Remove a flower" << endl;
   cout << "2. Add a flower" << endl;
   cout << "3. Checkout" << endl;
   cout << "4. Cancel" << endl;
+  cout << "Select an option: ";
   cin >> optionviewbouquet;
 }
+
+int optionplaceorder;
+void placeorder(){
+  cout << "1. Confirm Order" << endl;
+  cout << "2. Edit Cart" << endl;
+  cout << "3. Cancel" << endl;
+  cout << "Select an option: ";
+  cin >> optionplaceorder;
+}
+
 int main(){
 string bouquetName;
 
  cout << endl;
+    
     cout << "=== Welcome to Flower Shop ===" << endl;
     mainMenu();
-    if(optionMain == 1){
+    while(optionMain != 4){
+       if(optionMain == 1){
+        //open customerlogin.text
+        // ask for user login and proceed
         
         if(optionCustomer == 1){
           cout << "Name your bouquet: ";
           cin >> bouquetName;
           cout << "Available Flowers: ";
-            
+          //open flowers.txt
+        }
+
+        if(optionCustomer == 2){
+          cout << "--- Your Cart ---" << endl;
+          // open cart.txt
+        }
+
+        if(optionCustomer == 3){
+          cout << "--- Place Order ---" << endl;
+          // make the user choose a bouquet
+          // open bouquets.txt
+          // display bouquet info
+        }
+
+        if(optionCustomer == 4){
+          //open orderhistory.txt
+        }
+
+        if(optionCustomer == 5){
+          //write to customerlogin.txt to change profile info
+        }
+        
+        if(optionCustomer == 6){
+          cout << "Logged out!";
+          // repeat entire starting customer main code here
         }
     }
-
+    }
+   
   return 0;
 }
